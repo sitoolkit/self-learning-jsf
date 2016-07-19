31ce1fad477c2dd27ac0737988678cd985eed05a
hiroyuki.takeda <hiroyuki.takeda@monocrea.jp>
2016-07-19 12:41:16 +0900

    update l04sample

--- src/main/java/l04scope/L04ConversationScopedBean.java
+++ src/main/java/l04scope/L04ConversationScopedBean.java
@@ -1,7 +1,5 @@
 package l04scope;
 
-import java.io.Serializable;
-
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
@@ -9,24 +7,12 @@ import javax.inject.Named;
 
 @Named
 @ConversationScoped
-public class L04ConversationScopedBean implements Serializable {
+public class L04ConversationScopedBean extends L04ScopedBaseBean {
 
-	private int count;
+	private static final long serialVersionUID = 4386726676450777293L;
 
 	@Inject
 	private Conversation con;
-
-	public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public void countUp(){
-        count++;
-    }
 
 	public void start() {
 		if(con.isTransient()){
@@ -37,8 +23,9 @@ public class L04ConversationScopedBean implements Serializable {
 	public void end() {
 		con.end();
 	}
-		
+
     public boolean isState() {
         return con.isTransient();
     }
 }
+
